c9e5e88191ad1cab340c2233d604b4eb97985505
xiyiji <18310736878@163.com>
2021-12-20 01:21:59 +0800

    chore：修改了弹窗使用的库

--- core/P2P_Client/Client.py
+++ core/P2P_Client/Client.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 
 import redis
-from win10toast import ToastNotifier
+from plyer import notification
 
 
 class Client:
@@ -23,10 +23,6 @@ class Client:
             db=db)
         # 开启发布订阅模式
         self.pubsub = self.conn.pubsub()
-        # 创建一个win10弹窗实例
-        self.toaster = ToastNotifier()
-        # 用来接收订阅的消息（不过貌似没啥用）
-        self.msg = 0
 
     async def show(self, n_list):
         """
@@ -36,24 +32,25 @@ class Client:
                        n_list内容一般为{'appName':XXXX,'title':XXXX,'message':XXXX,'time':XXXX}
         """
         message = "{},{},{}".format(n_list["appName"], n_list["title"], n_list["message"])
+        title = "{}：{}".format(n_list["appName"], n_list["title"])
         print(message)
-        self.toaster.show_toast(n_list["appName"],  # 弹窗标题
-                                message,  # 弹窗内容
-                                icon_path=None,  # 弹窗图标（None就是python的图标）
-                                duration=5,  # 弹窗显示时间，在未显示完之前不会显示下一条内容
-                                threaded=True)  # True不会造成阻塞
+        notification.notify(title=title,  # 弹窗标题
+                            message=n_list["message"],  # 弹窗内容
+                            app_icon=None,  # 弹窗图标（None就是python的图标）
+                            timeout=30,  # 弹窗显示时间
+                            )
 
     async def recived(self):
         """
-        接受订阅频道的消息并通过win10弹窗显示
+        接受订阅频道的消息并通过弹窗显示
         """
         self.pubsub.subscribe("notification")  # 订阅一个名为“notification”的redis频道
         while True:
-            self.msg = self.pubsub.parse_response()  # 获取“notification”频道推送的内容
+            msg = self.pubsub.parse_response()  # 获取“notification”频道推送的内容
             print(time.strftime("%H:%M:%S", time.localtime()))
-            if type(self.msg[2]) is not int:  # 由于刚刚订阅时会推送一个1的int值，所以要判断一下
-                notice = eval(self.msg[2].decode('UTF-8'))  # 频道推送的内容是byte要转码
-                await self.show(n_list=notice)  # 调用win10弹窗方法
+            if type(msg[2]) is not int:  # 由于刚刚订阅时会推送一个1的int值，所以要判断一下
+                notice = eval(msg[2].decode('UTF-8'))  # 频道推送的内容是byte要转码
+                await self.show(n_list=notice)  # 调用弹窗方法
 
     def main(self):
         loop = asyncio.get_event_loop()
